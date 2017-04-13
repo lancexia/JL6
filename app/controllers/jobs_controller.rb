d60a60c377206819da555daf6b4520bd5cf0d656
lance <xiakailun919@163.com>
2017-04-13 18:29:57 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,7 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all.paginate(:page => params[:page], :per_page => 8)
+    @jobs = Job.paginate(:page => params[:page], :per_page => 8).where(:is_hidden => false).order("created_at DESC")
   end
 
   def new
@@ -54,7 +54,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 

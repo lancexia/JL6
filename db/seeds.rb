0767b6faf308f13c9c3622230873463bc0c091b3
lance <xiakailun919@163.com>
2017-04-23 06:45:12 +0800

    correct some index typesetting

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,22 +12,22 @@ puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 publi
 
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "柳叶公司.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(100..200)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "柳叶公司", location: "上海"])
+  Job.create!([title: "行政助手.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(100..200)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "柳叶公司", location: "上海", contact_email:"tget@162.com"])
 end
 puts "10 Public jobs created."
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "户文集团#{i}", description: "這是用種子建立的第 #{i} 個Hidden工作", wage_upper_bound: rand(20..60)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true", companyname: "户文集团", location: "南京"])
+  Job.create!([title: "专栏编辑#{i}", description: "這是用種子建立的第 #{i} 個Hidden工作", wage_upper_bound: rand(20..60)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true", companyname: "户文集团", location: "南京", contact_email:"yuan@163.com"])
 end
 puts "10 Hidden jobs created."
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "海茵文化.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..120)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "海茵文化", location: "杭州"])
+  Job.create!([title: "新闻记者.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..120)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "海茵文化", location: "杭州", contact_email:"aliy@gmail.com"])
 end
 puts "10 Public jobs created."
 
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "海茵文化.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..150)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "海茵文化", location: "杭州"])
+  Job.create!([title: "广告文案专员.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..150)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "海茵文化", location: "杭州", contact_email:"tito@okewen.com"])
 end
 puts "10 Public jobs created."

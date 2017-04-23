9daecbfb3b16f0fe8a1365275b463875766e5717
lance <xiakailun919@163.com>
2017-04-23 19:56:01 +0800

    to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 publi
 
 
 create_jos = for i in 1..10 do
-  Job.create!([title: "行政助手.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(100..200)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "柳叶公司", location: "上海", contact_email:"tget@162.com"])
+  Job.create!([title: "行政助手.#{i}", description: "两年以上的工作经验", wage_upper_bound: rand(100..200)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", companyname: "柳叶公司", location: "上海", contact_email:"tget@162.com"])
 end
 puts "10 Public jobs created."
 

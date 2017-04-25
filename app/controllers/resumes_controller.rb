fa68d2ae433516c3df4d9f489d842a0bb7d72119
lance <xiakailun919@163.com>
2017-04-25 19:33:32 +0800

    use bootstrap component to new job index

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,6 @@
 class ResumesController < ApplicationController
   before_action :authenticate_user!
+  
 
     def new
       @job = Job.find(params[:job_id])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def devise_mapping
       @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def newTimeFormat(time)
+   time.strftime("%Y-%m-%d")
+  end
 end
